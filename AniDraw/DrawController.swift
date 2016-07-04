e706ce9f08cc678a3aa84bb1ef7fbce4aedd3e38
s0294 <yanjingzz@gmail.com>
2016-07-04 15:03:58 +0800

    ColorPicker

--- AniDraw/DrawController.swift
+++ AniDraw/DrawController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MSColorPicker
 
 enum DrawingTool: Int {
     case Pencil = 1
@@ -16,14 +17,18 @@ enum DrawingTool: Int {
     case Eraser
 }
 
-class DrawController: UIViewController {
+class DrawController: UIViewController, MSColorSelectionViewControllerDelegate, UIPopoverPresentationControllerDelegate {
     
     @IBOutlet private weak var drawView: DrawView!
     @IBOutlet var toolsButton: [UIButton]!
+    @IBOutlet weak var selectedColorCircleView: UIImageView!
+    @IBOutlet weak var firstColorButton: UIButton!
     
+    var colorButton: UIButton?
     private struct Constant {
         static let drawingToolsAnimationDuration = 0.3
     }
+    
     
     var selectedTool = DrawingTool.Pencil {
         willSet {
@@ -88,6 +93,7 @@ class DrawController: UIViewController {
     
     private struct Storyborad {
         static let NextStepSegueIdentifier = "putSkeleton"
+        static let PopoverSegueIdentifier = "showPopover"
     }
     
     // MARK: - Navigation
@@ -105,6 +111,18 @@ class DrawController: UIViewController {
                 let image = drawView.image
                 skeletonVC.characterSkin = image
             }
+        case Storyborad.PopoverSegueIdentifier:
+            guard let destNav = segue.destinationViewController as? UINavigationController, let colorVC = destNav.visibleViewController as? MSColorSelectionViewController, let button = sender as? UIButton else{
+                break
+            }
+            self.colorButton = button
+            let popoverVC = destNav.popoverPresentationController! as UIPopoverPresentationController
+            popoverVC.sourceView = self.view
+            popoverVC.sourceRect = button.frame
+            popoverVC.presentedViewController.preferredContentSize = colorVC.view.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            destNav.popoverPresentationController?.delegate = self
+            colorVC.delegate = self
+            colorVC.color = UIColor.redColor()
         default:
             break
         }
@@ -112,5 +130,12 @@ class DrawController: UIViewController {
     
     @IBAction private func goBack(sender: UIButton) {
         dismissViewControllerAnimated(true) {}
+    }
+    
+    func colorViewController(colorViewCntroller: MSColorSelectionViewController!, didChangeColor color: UIColor!) {
+        guard let button = colorButton, let c = color else {
+            return
+        }
+        button.backgroundColor = c
     }
 }
--- Podfile
+++ Podfile
@@ -5,4 +5,5 @@ platform :ios, '9.0'
 target 'AniDraw' do
 	pod 'Beethoven'
 	pod 'JGProgressHUD'
+	pod "MSColorPicker"
 end
